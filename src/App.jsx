177d8a1e6886e43a5e7e69706780514eb55eabc6
Brice Kelly <ntirukelly@gmail.com>
2023-12-08 05:34:22 -0800

    setup plus pages

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+     <h1>UseState Practice</h1>
+      <div className="App">
+        <button>Counter</button>
+        <button>Controlled input field</button>
+        <button>Toggle Visibility</button>
+        <button>Character counter</button>
+        <button>Todo List</button>
+        <button>Color Switcher</button>
+        <button>Search filter</button>
+        <button>Drag and drop list</button>
+      </div>
+    </>
+  )
+}
+
+export default App;
